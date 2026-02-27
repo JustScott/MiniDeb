a0a9e8459d253b54cec97d733d0708d3c79efc38
JustScott <development@justscott.me>
2026-02-27 12:37:16 -0600

    Implement completion tracking

--- README.md
+++ README.md
@@ -2,4 +2,30 @@
 
 Scripts to setup a minimal, opinionated, Gnome-based Debian Linux system.
 
+## Usage
 
+First, ensure you are connected to Ethernet or WiFi. Then clone the repository 
+and run `start_install.sh`:
+
+```bash
+apt update
+apt install git
+git clone https://www.github.com/JustScott/MiniDeb
+bash ./MiniDeb/start_install.sh
+```
+
+### Script Completion Handling
+A `start_install_completion.txt` file is created as the script is
+ran so that if any command in the script fails, you can resolve
+the issue and run `bash ./MiniDeb/start_install.sh` again for it
+to start after the last successfully ran command.
+  * If you want to restart the script, just delete the completion
+    file
+  * If you want to start from a specific command/position in the 
+    script, you can just delete up to and including that command
+  * Or you can even just delete a specific command to just rerun
+    that command
+
+There is also a `finish_install_completion.txt` file located at /mnt/
+that tracks the completion of `finish_install.sh`. All the rules above
+regarding `start_install` apply the same.
--- finish_install.sh
+++ finish_install.sh
@@ -23,6 +23,16 @@ INSTALLATION_VARIABLES_FILE=/activate_installation_variables.sh
 
 PRETTY_OUTPUT_LIBRARY=/pretty_output_library.sh
 
+COMPLETION_FILE="/finish_install_completion.txt"
+
+export DEBIAN_FRONTEND=noninteractive
+
+if [[ "$(whoami)" != "root" ]]
+then
+    printf "\n\e[31m%s\e[0m\n" "[!] Must run script as root"
+    exit 1
+fi
+
 if ! source $PRETTY_OUTPUT_LIBRARY &>/dev/null
 then
     printf "\n\n\e[31m%s %s\e[0m\n\n" \
@@ -53,28 +63,45 @@ then
     exit 1
 fi
 
+if ! grep "^apt_update$" $COMPLETION_FILE &>/dev/null
+then
+    apt-get update >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Update apt"
+    [[ $? -ne 0 ]] && exit 1
 
-apt update >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Update apt"
-[[ $? -ne 0 ]] && exit 1
+    echo "apt_update" >> $COMPLETION_FILE
+fi
 
+debconf-set-selections > /dev/null 2>&1 <<EOF
+keyboard-configuration keyboard-configuration/layoutcode string us
+keyboard-configuration keyboard-configuration/modelcode string pc105
+keyboard-configuration keyboard-configuration/variantcode string
+console-setup console-setup/charmap47 select UTF-8
+EOF
 
-apt install -y gnome locales efibootmgr efivar linux-image-amd64 \
-    grub-efi-amd64-bin network-manager sudo \
-    >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Install system packages and gnome"
-[[ $? -ne 0 ]] && exit 1
+if ! grep "^apt_install_system_packages$" $COMPLETION_FILE &>/dev/null
+then
+    apt-get install --yes gnome locales efibootmgr efivar linux-image-amd64 \
+        grub-efi-amd64-bin network-manager sudo \
+        >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Install system packages and gnome"
+    [[ $? -ne 0 ]] && exit 1
 
+    echo "apt_install_system_packages" >> $COMPLETION_FILE
+fi
+
+# TODO: Implement checks for locale and timezone to see if they've been set
 {
     # Set the keyboard orientation
-    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-    export LANG="en_US.UTF-8"
-    echo "LANG=$LANG" > /etc/locale.conf
+    echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" > /etc/default/locale
+    update-locale LANG=en_US.UTF-8
 } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
 task_output $! "$STDERR_LOG_PATH" "Configure locale: 'en_US.UTF-8'"
 [[ $? -ne 0 ]] && exit 1
 
-if [[ -n "$user_timezone" ]]
+if ! cmp -s /usr/share/zoneinfo/America/Chicago /etc/localtime &>/dev/null
 then
     ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime \
         >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
@@ -82,47 +109,100 @@ then
     [[ $? -ne 0 ]] && exit 1
 fi
 
-locale-gen >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Generate locale"
-[[ $? -ne 0 ]] && exit 1
+if ! grep "^generate_locale$" $COMPLETION_FILE &>/dev/null
+then
+    locale-gen >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Generate locale"
+    [[ $? -ne 0 ]] && exit 1
 
-grub-install --efi-directory=/boot \
-    >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Install grub"
-[[ $? -ne 0 ]] && exit 1
+    echo "generate_locale" >> $COMPLETION_FILE
+fi
 
-grub-mkconfig -o /boot/grub/grub.cfg \
-    >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Configure grub"
+if ! grep "^grub_install$" $COMPLETION_FILE &>/dev/null
+then
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi \
+        --bootloader-id=Debian $disk \
+        >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Install grub"
+    [[ $? -ne 0 ]] && exit 1
 
-update-initramfs -u \
-    >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Update the initramfs"
+    echo "grub_install" >> $COMPLETION_FILE
+fi
 
-useradd -UmG sudo -s /bin/bash administrator \
-    >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Create administrator account"
-[[ $? -ne 0 ]] && exit 1
+if ! grep "^grub_mkconfig$" $COMPLETION_FILE &>/dev/null
+then
+    grub-mkconfig -o /boot/grub/grub.cfg \
+        >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Configure grub"
 
+    echo "grub_mkconfig" >> $COMPLETION_FILE
+fi
+
+if ! grep "^update_initramfs$" $COMPLETION_FILE &>/dev/null
+then
+    update-initramfs -u >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Update the initramfs"
+
+    echo "update_initramfs" >> $COMPLETION_FILE
+fi
+
+if ! id administrator &>/dev/null
+then
+    useradd -Um -s /bin/bash administrator \
+        >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create administrator account"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+# Double check the admin account is created
+if ! id administrator &>/dev/null
+then
+    printf "\n\e[31m%s %s\e[0m\n" "[!] user 'administrator' doesn't exist..." \
+        "this shouldn't happen... stopping"
+    exit 1
+fi
+
+if ! groups administrator | grep "sudo" &>/dev/null
+then
+    usermod -aG sudo administrator >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Add administrator to the sudo group"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+# No need for completion tracking, just set the password
 echo administrator:"$admin_password" | chpasswd \
     >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
 task_output $! "$STDERR_LOG_PATH" "Set administrator password"
 [[ $? -ne 0 ]] && exit 1
 
-useradd -Um -s /bin/bash $username  \
-    >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-task_output $! "$STDERR_LOG_PATH" "Create user account: '$username'"
-[[ $? -ne 0 ]] && exit 1
+if ! id $username &>/dev/null
+then
+    useradd -Um -s /bin/bash $username  \
+        >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create user account: '$username'"
+    [[ $? -ne 0 ]] && exit 1
+fi
 
+# Double check the user account is created
+if ! id $username &>/dev/null
+then
+    printf "\n\e[31m%s %s\e[0m\n" "[!] user '$username' doesn't exist..." \
+        "this shouldn't happen... stopping"
+    exit 1
+fi
+
+# No need for completion tracking
 passwd -d $username >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
 task_output $! "$STDERR_LOG_PATH" "Make user account passwordless"
 [[ $? -ne 0 ]] && exit 1
 
-echo -e "[User]\nUserAccount=true" \
+# No need for completion tracking
+echo -e "[User]\nSystemAccount=true" \
     > /var/lib/AccountsService/users/administrator &
 task_output $! "$STDERR_LOG_PATH" "Remove administrator from gdm login screen"
 [[ $? -ne 0 ]] && exit 1
 
+# No need for completion tracking
 systemctl enable NetworkManager >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
 task_output $! "$STDERR_LOG_PATH" "Enable NetworkManager service"
 [[ $? -ne 0 ]] && exit 1
