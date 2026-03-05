2a3a1da2d1a00ce4022f00d3019c31e7ea6510cb
JustScott <development@justscott.me>
2026-03-04 20:24:59 -0600

    Accidently used AGPLv3 in script license notices, changed them to GPLv3 and updated with my real name and real name email

--- finish_install.sh
+++ finish_install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # finish_install.sh - part of the DebianInstaller project
-# Copyright (C) 2026, JustScott, development@justscott.me
+# Copyright (C) 2026, Scott Wyman, development@scottwyman.me
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 disk="/dev/vda"
--- pretty_output_library.sh
+++ pretty_output_library.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-# pretty_output_library - part of the DebianInstaller project
-# Copyright (C) 2026, JustScott, development@justscott.me
+# pretty_output_library.sh - part of the DebianInstaller project
+# Copyright (C) 2026, Scott Wyman, development@scottwyman.me
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 STDOUT_LOG_PATH="/dev/null"
