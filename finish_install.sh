a9f879ad4e6c4e565cc88edd47490cb9c87c2ba1
JustScott <development@justscott.me>
2026-03-13 10:50:46 -0500

    Install more fonts

--- finish_install.sh
+++ finish_install.sh
@@ -118,7 +118,9 @@ then
     task_output $! "$STDERR_LOG_PATH" "Install gnome"
     [[ $? -ne 0 ]] && exit 1
 
-    apt-get install --yes locales neovim curl wget \
+    apt-get install --yes \
+        fonts-recommended fonts-noto* \
+        locales neovim curl wget \
         git efibootmgr efivar linux-image-amd64 \
         grub-efi-amd64-bin network-manager sudo \
         plymouth plymouth-themes \
