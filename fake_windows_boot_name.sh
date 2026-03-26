9ead9753def7a34be696a74c2e25e6bbb3dc14fe
JustScott <development@justscott.me>
2026-03-25 19:14:31 -0500

    Add script for fixing persistant install issues on some systems

---
+++ fake_windows_boot_name.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# fake_windows_boot_name.sh - part of the DebianInstaller project
+# Copyright (C) 2026, Scott Wyman, development@scottwyman.me
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Sometimes motherboard creators only allow booting from partition with 
+# the label "Windows Boot Manager"
+
+sudo efibootmgr -c -L "Windows Boot Manager" -l "\EFI\Debian\grubx64.efi"
